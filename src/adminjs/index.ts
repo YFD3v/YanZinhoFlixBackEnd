8f73b380a77d19b48d0ff909568a9a2e9f57fd77
Yan Ferreira <fabioyan2@gmail.com>
2024-01-16 14:33:18 -0300

    Personalizando o dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,7 +5,7 @@ import { default as AdminJsExpress } from "@adminjs/express";
 import { default as AdminJsSequelize } from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { User } from "../models";
+import { Category, Course, Episode, User } from "../models";
 import bcrypt from "bcrypt";
 import { locale } from "./locale";
 
@@ -39,6 +39,23 @@ export const adminJs = new AdminJS({
   },
   //Passo 12 adicionando tradução
   locale: locale,
+  //Passo 13 personalizando o dashboard
+  dashboard: {
+    component: AdminJS.bundle("./components/Dashboard"),
+    handler: async (req, res, context) => {
+      const courses = await Course.count();
+      const episodes = await Episode.count();
+      const categories = await Category.count();
+      const standardUsers = await User.count({ where: { role: "user" } });
+
+      res.json({
+        Cursos: courses,
+        Episódios: episodes,
+        Categorias: categories,
+        Usuários: standardUsers,
+      });
+    },
+  },
 });
 //Anteriormente era: export const adminJsRouter = AdminJsExpress.buildRouter(adminjs)
 //Passo 11 - autenticação e tela de login
